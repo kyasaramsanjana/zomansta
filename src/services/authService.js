7a62b8d5e6a8580ee06dd4df4f1362655ba52a2f
kyasaramsanjana <kyasaramsanajana57@gmail.com>
2026-07-07 14:49:47 +0530

    Add authService with register and login logic

---
+++ src/services/authService.js
@@ -0,0 +1,54 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const generateToken = require('../utils/generateToken');
+
+const registerUser = async (name, email, password) => {
+  // Check if user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    throw new Error('User already exists');
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user in database
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  });
+
+  return {
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token: generateToken(user._id)
+  };
+};
+
+const loginUser = async (email, password) => {
+  // Find user by email
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error('Invalid email or password');
+  }
+
+  // Check password
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error('Invalid email or password');
+  }
+
+  return {
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token: generateToken(user._id)
+  };
+};
+
+module.exports = { registerUser, loginUser };
