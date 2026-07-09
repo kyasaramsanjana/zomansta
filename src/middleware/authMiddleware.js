46945597019ceec986e0ac45f5de3abcb72f30d1
Kyasaram Sanjana <kyasaramsanjana57@gmail.com>
2026-07-09 22:59:25 +0530

    Fix formatting in authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,7 +4,6 @@ const User = require('../models/User');
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
-
   if (req.headers.authorization && 
       req.headers.authorization.startsWith('Bearer')) {
     token = req.headers.authorization.split(' ')[1];
