96ca6fc8e70177a6c3a08373bc5b3faaef10f165
Kyasaram Sanjana <kyasaramsanjana57@gmail.com>
2026-07-12 23:37:34 +0530

    updated reelController.js
    Removed route comments for clarity in reelController.js

--- src/controllers/reelController.js
+++ src/controllers/reelController.js
@@ -8,7 +8,6 @@ const {
   deleteReel
 } = require('../services/reelService');
 
-// @route POST /api/reels
 const uploadReel = asyncHandler(async (req, res) => {
   const { title, description, videoUrl, thumbnail, restaurant, tags } = req.body;
 
@@ -30,31 +29,25 @@ const uploadReel = asyncHandler(async (req, res) => {
   res.status(201).json({ success: true, data: reel });
 });
 
-// @route GET /api/reels
 const getReels = asyncHandler(async (req, res) => {
   const reels = await getAllReels();
   res.status(200).json({ success: true, data: reels });
 });
 
-// @route GET /api/reels/:id
 const getReel = asyncHandler(async (req, res) => {
   const reel = await getReelById(req.params.id);
   res.status(200).json({ success: true, data: reel });
 });
 
-// @route PUT /api/reels/:id/like
 const toggleLike = asyncHandler(async (req, res) => {
   const reel = await likeReel(req.params.id, req.user._id.toString());
   res.status(200).json({ success: true, data: reel });
 });
 
-// @route PUT /api/reels/:id/save
 const toggleSave = asyncHandler(async (req, res) => {
   const reel = await saveReel(req.params.id, req.user._id.toString());
   res.status(200).json({ success: true, data: reel });
 });
-
-// @route DELETE /api/reels/:id
 const removeReel = asyncHandler(async (req, res) => {
   const result = await deleteReel(req.params.id, req.user._id.toString());
   res.status(200).json({ success: true, data: result });
