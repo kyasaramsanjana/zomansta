32774f3fac22b7b9fae2eb5a456c6a3eb614477b
Kyasaram Sanjana <kyasaramsanjana57@gmail.com>
2026-07-14 21:51:54 +0530

    Refactor authService.js for code cleanliness
    Removed extra newlines for cleaner code formatting.

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const generateToken = require('../utils/generateToken');
-
 const registerUser = async (name, email, password) => {
   const userExists = await User.findOne({ email });
   if (userExists) {
@@ -9,13 +8,11 @@ const registerUser = async (name, email, password) => {
   }
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
-
   const user = await User.create({
     name,
     email,
     password: hashedPassword
   });
-
   return {
     _id: user._id,
     name: user.name,
@@ -24,19 +21,15 @@ const registerUser = async (name, email, password) => {
     token: generateToken(user._id)
   };
 };
-
 const loginUser = async (email, password) => {
-
   const user = await User.findOne({ email });
   if (!user) {
     throw new Error('Invalid email or password');
   }
-
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) {
     throw new Error('Invalid email or password');
   }
-
   return {
     _id: user._id,
     name: user.name,
@@ -45,5 +38,4 @@ const loginUser = async (email, password) => {
     token: generateToken(user._id)
   };
 };
-
 module.exports = { registerUser, loginUser };
