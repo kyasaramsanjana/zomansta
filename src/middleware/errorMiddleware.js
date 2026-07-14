73955c7c8f6e9ebca613dcb0faf79eacf762c07f
Kyasaram Sanjana <kyasaramsanjana57@gmail.com>
2026-07-14 21:50:52 +0530

    Refactor errorHandler for consistency

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,10 +1,8 @@
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  
+const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode).json({
     success: false,
     message: err.message
   });
 };
-
 module.exports = { errorHandler };
