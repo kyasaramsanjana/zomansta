6069c39e88e85a04b74e28fee131e16ffe67c867
Kyasaram Sanjana <kyasaramsanjana57@gmail.com>
2026-07-17 23:59:49 +0530

    Remove unnecessary newline in app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ const { errorHandler } = require('./middleware/errorMiddleware');
 const app = express();
 app.use(helmet());
 app.use(cors());
-
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100
