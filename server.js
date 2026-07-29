185f38bfe128cda2f2bf36d7a20edd9331741248
Kyasaram Sanjana <kyasaramsanjana57@gmail.com>
2026-07-29 21:55:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const dotenv = require('dotenv');
 const connectDB = require('./src/config/db');
-
 dotenv.config();
 connectDB();
-
 const app = require('./src/app');
-
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
